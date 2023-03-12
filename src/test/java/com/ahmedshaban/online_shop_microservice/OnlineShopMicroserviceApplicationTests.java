55155da37bf1d77c4888d2fc64153200a0fcf09c
Ahmed Shaban <ahmed.shaaban.elhdah@gmail.com>
2023-03-12 12:02:50 +0200

    kickoff commit

---
+++ src/test/java/com/ahmedshaban/online_shop_microservice/OnlineShopMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmedshaban.online_shop_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
