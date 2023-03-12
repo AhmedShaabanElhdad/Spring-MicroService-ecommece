274cda6483f75a5b1693dd3cf679fd1616014061
Ahmed Shaban <ahmed.shaaban.elhdah@gmail.com>
2023-03-12 17:23:31 +0200

    get place order after check order line in stock
    - add WebClientConfig for service communication
    - change the api for invertory to return list of inventory response
    - check order line before place order

--- Inventory-service/src/main/java/com/ahmedshaban/inventoryservice/repository/InventoryRepository.java
+++ Inventory-service/src/main/java/com/ahmedshaban/inventoryservice/repository/InventoryRepository.java
@@ -4,9 +4,10 @@ import com.ahmedshaban.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
-    Optional<Inventory> findBySku(String sku);
+    List<Inventory> findBySkuIn(List<String> sku);
 }
--- Order-service/src/main/java/com/ahmedshaban/orderservice/service/OrderService.java
+++ Order-service/src/main/java/com/ahmedshaban/orderservice/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.ahmedshaban.orderservice.service;
 
 
+import com.ahmedshaban.orderservice.dto.InventoryResponse;
 import com.ahmedshaban.orderservice.dto.OrderLineItemDto;
 import com.ahmedshaban.orderservice.dto.OrderRequest;
 import com.ahmedshaban.orderservice.model.Order;
@@ -9,7 +10,9 @@ import com.ahmedshaban.orderservice.repository.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -20,13 +23,35 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
+
+        // get all Order line
         List<OrderLineItem> orderLineItems = orderRequest.getOrderLine().stream().map(this::mapToOrderLine).collect(Collectors.toList());
         order.setOrderLineItems(orderLineItems);
-        orderRepository.save(order);
+
+        // get all sku from order line
+        List<String> skuList = order.getOrderLineItems()
+                .stream()
+                .map(OrderLineItem::getSku)
+                .collect(Collectors.toList());
+
+        //check quantity for item in stock using inventory service
+        InventoryResponse[] inventoryResponses = webClient.get()
+                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("sku", skuList).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean isInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::getIsInStock);
+
+        if (isInStock)
+            orderRepository.save(order);
+        else
+            throw new IllegalArgumentException("product in not in stock");
 
     }
 
